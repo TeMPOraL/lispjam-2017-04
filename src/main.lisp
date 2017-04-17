ae7ba90c14fdf74968091d3ac7a0ecef2e5a2612
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2017-04-17 15:13:50 +0200

    Basic engine use.
    + gitignore

--- src/main.lisp
+++ src/main.lisp
@@ -3,6 +3,66 @@
 (defclass ljgame (p2d:game)
   ())
 
+
+
+(defmethod p2d:preinit ((game ljgame))
+  (setf p2d:*window-title* "#:LISPJAM-2017-04-GAME1000"
+        p2d:*use-fixed-timestep* t
+        p2d:*window-width* 1280
+        p2d:*window-height* 1024
+        p2d:*window-resizable* nil))
+
+(defmethod p2d:initialize ((game ljgame))
+  (log:info "Initializing game.")
+
+  ;; Graphics defaults
+  (gl:clear-color 0.0 0.0 0.0 1.0)
+
+  ;; (sdl2:gl-set-swap-interval 0) <-- use to disable vsync
+
+  (gl:blend-func :src-alpha :one-minus-src-alpha)
+  (gl:enable :blend)
+  (gl:enable :line-smooth)
+  (gl:hint :line-smooth-hint :nicest)
+  (gl:enable :polygon-smooth)
+  (gl:hint :polygon-smooth-hint :nicest))
+
+(defmethod p2d:deinitialize ((game ljgame))
+  ;; TODO
+  (log:info "Game deinitialized.")
+  )
+
+(defmethod p2d:on-key-event ((game ljgame) key state repeat)
+  (macrolet ((on-key-down (scancode &body code)
+               `(when (and (eql key-code ,scancode)
+                           (sdl2:key-down-p state))
+                  ,@code)))
+    (let ((key-code (sdl2:scancode-symbol (sdl2:scancode-value key))))
+      (log:trace key state key-code repeat)
+
+      ;; direct keydown events go here
+      (on-key-down :scancode-escape
+                   (sdl2:push-event :quit)))))
+
+(defmethod p2d:on-tick ((game ljgame) dt)
+  ;; TODO
+  )
+
+(defmethod p2d:on-idle ((game ljgame) dt)
+  ;; TODO
+  )
+
+(defmethod p2d:on-render ((game ljgame) dt)
+  ;; TODO
+
+  (p2dg:with-color (1 0 0)
+    (gl:with-pushed-matrix
+      (gl:translate 400 300 0)
+      (gl:scale 10 10 10)
+      (p2dglu:draw-triangle))))
+
+
+
 (defun run ()
   (p2d:run :game (make-instance 'ljgame)))
 
